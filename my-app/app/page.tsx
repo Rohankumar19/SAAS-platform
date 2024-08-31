460c2434984e1943559293b5d05b1406836f7422
Rohan Kumar <rohankumar19003@gmail.com>
2024-08-31 14:27:11 +0530

    chore: Update authentication API route to export GET and POST handlers separately

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+console.log(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID)
+console.log(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET)
 export default function Home() {
   return (
     <main>
