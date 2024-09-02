0d2bdd6faba5829892dad9407d750589d71276f4
Rohan Kumar <rohankumar19003@gmail.com>
2024-09-02 11:25:50 +0530

    updated appbar

--- my-app/app/components/Appbar.tsx
+++ my-app/app/components/Appbar.tsx
@@ -1,9 +1,6 @@
 export function Appbar() {
     return <div>
-        <div className="flex  justify-between" style={{
-            display: "flex",
-            justifyContent: "space-between",
-        }}>
+        <div className="flex  justify-between">
             muzi
         </div>
     </div>
