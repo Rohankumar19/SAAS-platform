758df3ae640aa38a4e23a50ea3559b0e6b230353
Rohan Kumar <rohankumar19003@gmail.com>
2024-09-04 10:13:00 +0530

    updated version

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Appbar } from "./components/Appbar";
 console.log(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID)
 console.log(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET)
 export default function Home() {
