054cb0d24ea7af70c6c71b4001dd13f3502dfc19
Rohan Kumar <rohankumar19003@gmail.com>
2024-09-04 10:35:53 +0530

    wrapped

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <SessionProvider>
+        {children}
+      </SessionProvider>
     </html>
   );
 }
