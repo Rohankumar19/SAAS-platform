56965ef51a4f9fc780af77b6a036b31737020808
Rohan Kumar <rohankumar19003@gmail.com>
2024-09-06 10:25:24 +0530

    added new schema

--- my-app/app/api/Streams/route.ts
+++ my-app/app/api/Streams/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from "next/server";
 import { z } from "zod";
 
-
+const CreateStreamSchema = z.object()
 
 export function POST(req: NextRequest){
     const data = await req.json();
