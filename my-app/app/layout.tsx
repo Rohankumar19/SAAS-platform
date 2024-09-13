373cdd2850be460ab9f9707c9b0150dd3f3208a0
Rohan Kumar <rohankumar19003@gmail.com>
2024-09-13 10:50:52 +0530

    updated layout.tsx and page.tsx

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
-import Providers from "./provider";
+import { Providers } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Providers>
-        {children}
-      </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
