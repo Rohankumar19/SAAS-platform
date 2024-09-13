94d42b24fd08baf60cf685ac2a7ef4ff4240685c
Rohan Kumar <rohankumar19003@gmail.com>
2024-09-13 10:21:07 +0530

    added dashboard

---
+++ my-app/app/dashboard/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useEffect } from "react";
+import { useSocket } from "@/context/socket-context";
+import useRedirect from "../../hooks/useRedirect";
+import jwt from "jsonwebtoken";
+import StreamView from "../../components/StreamView";
+
+export default function Component() {
+  const { socket, user, connectionError } = useSocket();
+  useRedirect();
+
+  useEffect(() => {
+    if (user) {
+      const token = jwt.sign(
+        {
+          creatorId: user?.id,
+          userId: user?.id,
+        },
+        process.env.NEXT_PUBLIC_SECRET || "",
+        {
+          expiresIn: "24h",
+        },
+      );
+
+      socket?.send(
+        JSON.stringify({
+          type: "join-room",
+          data: {
+            token,
+          },
+        }),
+      );
+    }
+  }, [user]);
+
+  if (connectionError) {
+    return <h1>Cannot connect to socket server</h1>;
+  }
+
+  if (!user) {
+    return <h1>Please Log in....</h1>;
+  }
+
+  return <StreamView creatorId={user.id} playVideo={true} />;
+}
+
+export const dynamic = "auto";
